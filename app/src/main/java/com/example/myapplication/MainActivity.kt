8eaa42cd6356a7538f12db97de7db076dbab5ca4
RGBRacoon <113908626+GRBRacoon@users.noreply.github.com>
2023-11-07 17:47:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import dji.v5.common.error.IDJIError
+import dji.v5.common.register.DJISDKInitEvent
+import dji.v5.manager.SDKManager
+import dji.v5.manager.interfaces.SDKManagerCallback
+
+class MainActivity : AppCompatActivity() {
+    private val TAG= this::class.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        registerApp()
+    }
+    private fun registerApp()
+    {
+        SDKManager.getInstance().init(this,object: SDKManagerCallback {
+            override fun onInitProcess(event: DJISDKInitEvent?, totalProcess: Int) {
+                Log.i(TAG, "onInitProcess: ")
+                if (event == DJISDKInitEvent.INITIALIZE_COMPLETE) {
+                    SDKManager.getInstance().registerApp()
+                }
+            }
+            override fun onRegisterSuccess() {
+                Log.i(TAG, "onRegisterSuccess: ")
+                /*0val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.content,SimulatorFragment())
+                transaction.commit()*/
+            }
+            override fun onRegisterFailure(error: IDJIError?) {
+                Log.i(TAG, "onRegisterFailure: ")
+            }
+            override fun onProductConnect(productId: Int)
+            {
+                Log.i(TAG, "onProductConnect: ")
+            }
+            override fun onProductDisconnect(productId: Int) {
+                Log.i(TAG, "onProductDisconnect: ")
+            }
+            override fun onProductChanged(productId: Int)
+            {
+                Log.i(TAG, "onProductChanged: ")
+            }
+            override fun
+                    onDatabaseDownloadProgress(current: Long, total: Long) {
+                Log.i(TAG, "onDatabaseDownloadProgress: ${current/total}")
+            }
+        })
+    }
+}
